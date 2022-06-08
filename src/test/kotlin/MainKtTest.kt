12a159f1abc90c953f3f78f8b8c5d0fb7e55736c
MariSova <bk.1901@bk.ru>
2022-06-08 16:21:47 +0300

    edits

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -8,6 +8,22 @@ class MainKtTest {
     @Test
     fun calculateCommission() {
         val card = "Vk Pay"
+        val month = 61000
+        val transfer = 1500
+
+        val result = calculateCommission(
+            userCard = card,
+            transferAmountMonth = month,
+            transferAmountUser = transfer
+        )
+
+        assertEquals(1125, result)
+
+    }
+
+    @Test
+    fun calculateCommissionVk() {
+        val card = "Vk Pay"
         val month = 0
         val transfer = 10000
 
@@ -38,10 +54,42 @@ class MainKtTest {
     }
 
     @Test
+    fun calculateCommissionVisaZeroMonth() {
+        val card = "Visa"
+        val month = 0
+        val transfer = 1700
+
+        val result = calculateCommission(
+            userCard = card,
+            transferAmountMonth = month,
+            transferAmountUser = transfer
+        )
+
+        assertEquals(1275, result)
+
+    }
+
+    @Test
     fun calculateCommissionMaster() {
         val card = "MasterCard, Maestro"
         val month = 790
         val transfer = 100
+
+        val result = calculateCommission(
+            userCard = card,
+            transferAmountMonth = month,
+            transferAmountUser = transfer
+        )
+
+        assertEquals(0, result)
+
+    }
+
+    @Test
+    fun calculateCommissionMastercard() {
+        val card = "MasterCard, Maestro"
+        val month = 0
+        val transfer = 900
 
         val result = calculateCommission(
             userCard = card,
